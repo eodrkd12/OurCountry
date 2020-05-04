f6b14d31b4dc476742434965febe20e3e66a6110
lilly9928 <50159938+lilly9928@users.noreply.github.com>
2020-05-04 17:30:56 +0900

    카테고리완료 마이페이지 수정중

--- app/src/main/java/com/honestyandpassion/ourcountry/IntroActivity/SplashActivity.kt
+++ app/src/main/java/com/honestyandpassion/ourcountry/IntroActivity/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.honestyandpassion.ourcountry.IntroActivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.honestyandpassion.ourcountry.MainActivity.MainActivity
 import com.honestyandpassion.ourcountry.R
 
 class SplashActivity : AppCompatActivity() {
@@ -9,5 +12,11 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(baseContext, MainActivity::class.java))
+            finish()
+        }, 2000L)
     }
+
 }
