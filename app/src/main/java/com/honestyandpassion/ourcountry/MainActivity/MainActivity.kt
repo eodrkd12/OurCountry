f9f65676c6b0b1900aaa238feef749038b62bf1f
sorrylim <52535550+sorrylim@users.noreply.github.com>
2020-05-04 15:55:58 +0900

    2020.05.04
    15:55

--- app/src/main/java/com/honestyandpassion/ourcountry/MainActivity/MainActivity.kt
+++ app/src/main/java/com/honestyandpassion/ourcountry/MainActivity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.honestyandpassion.ourcountry.IntroActivity
+package com.honestyandpassion.ourcountry.MainActivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
