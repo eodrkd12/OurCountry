e616a8c3f61b2e33bb2d5551d6760e01b09ca48b
sorrylim <52535550+sorrylim@users.noreply.github.com>
2020-05-06 14:56:48 +0900

    2020.05.06
    14:56

--- app/src/main/java/com/honestyandpassion/ourcountry/IntroActivity/SplashActivity.kt
+++ app/src/main/java/com/honestyandpassion/ourcountry/IntroActivity/SplashActivity.kt
@@ -1,7 +1,9 @@
 package com.honestyandpassion.ourcountry.IntroActivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.honestyandpassion.ourcountry.MainActivity.MainActivity
 import com.honestyandpassion.ourcountry.R
 
 class SplashActivity : AppCompatActivity() {
@@ -9,5 +11,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        var intent= Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
