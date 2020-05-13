686c735b256b77be44bbbaa3181b495bfa5411f4
eodrkd12 <eodrkd12@gmail.com>
2020-05-13 15:27:20 +0900

    회원가입 개발중

--- Application/app/src/main/java/com/honestyandpassion/ourcountry/IntroActivity/Join3Activity.kt
+++ Application/app/src/main/java/com/honestyandpassion/ourcountry/IntroActivity/Join3Activity.kt
@@ -1,13 +1,33 @@
 package com.honestyandpassion.ourcountry.IntroActivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.honestyandpassion.ourcountry.R
+import kotlinx.android.synthetic.main.activity_join3.*
 
 class Join3Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join3)
+
+        var email=intent.getStringExtra("email")
+        var nickname=intent.getStringExtra("nickname")
+        var password=intent.getStringExtra("password")
+
+        var intent= Intent(this,Join4Activity::class.java)
+        intent.putExtra("email",email)
+        intent.putExtra("nickname",nickname)
+        intent.putExtra("password",password)
+
+        btn_buyer.setOnClickListener {
+            intent.putExtra("user_type","buyer")
+            startActivity(intent)
+        }
+        btn_seller.setOnClickListener {
+            intent.putExtra("user_type","seller")
+            startActivity(intent)
+        }
     }
 }
