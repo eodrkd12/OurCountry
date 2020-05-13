4a3a8f34aeb07fa099e1e07312c7aa0f36ee1ea7
eodrkd12 <eodrkd12@gmail.com>
2020-05-13 14:21:53 +0900

    asdf
    safd

--- Application/app/src/main/java/com/honestyandpassion/ourcountry/IntroActivity/Join2Activity.kt
+++ Application/app/src/main/java/com/honestyandpassion/ourcountry/IntroActivity/Join2Activity.kt
@@ -3,6 +3,7 @@ package com.honestyandpassion.ourcountry.IntroActivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.common.api.Scope
 import com.honestyandpassion.ourcountry.R
 import kotlinx.android.synthetic.main.activity_join2.*
 
@@ -16,10 +17,12 @@ class Join2Activity : AppCompatActivity() {
         var nickname=intent.getStringExtra("nickname")
         var password=intent.getStringExtra("password")
 
-
-
         btn_next.setOnClickListener {
 
         }
+    }
+
+    fun sendMail(email:String){
+
     }
 }
