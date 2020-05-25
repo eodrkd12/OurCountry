c6182e7d205b7c395cb012b7befd7c6dd738a12e
sorrylim <52535550+sorrylim@users.noreply.github.com>
2020-05-25 14:20:20 +0900

    2020.05.25
    14:20

---
+++ Server/public/SQL/wishlist_sql.js
@@ -0,0 +1,27 @@
+var pool=require('../../config/db_config');
+
+module.exports=function(){
+    return {
+        insert_wishlist : function(registerId, userId, callback){
+            pool.getConnection(function(err, con){
+                var sql=`insert into Wishlist values(${registerId}, '${userId}')`
+                con.query(sql, function(err, result, fields){
+                    con.release()
+                    if(err) console.log(err)
+                    else console.log("위시리스트 등록 완료")
+                })
+            })
+        },
+        delete_wishlist : function(registerId, userId, callback){
+            pool.getConnection(function(err, con){
+                var sql=`delete from Wishlist where register_id=${registerId} and user_id='${userId}'`
+                con.query(sql, function(err, result, fields){
+                    con.release()
+                    if(err) console.log(err)
+                    else console.log("위시리스트 삭제 완료")
+                })
+            })
+        },
+        pool: pool
+    }
+};
