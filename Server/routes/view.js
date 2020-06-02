22471f4e07ebdebe6b3f16434a33f7320f01fef3
sorrylim <52535550+sorrylim@users.noreply.github.com>
2020-06-02 16:46:28 +0900

    2020.06.02
    16:46

--- Server/routes/view.js
+++ Server/routes/view.js
@@ -7,8 +7,9 @@ router.post('/insert', function(req,res,next){
   var registerId=req.body.register_id
   var userId=req.body.user_id
   var viewDate=req.body.view_date
+  var registerTitle=req.body.register_title
 
-  db_view.insert_view(userId, registerId, viewDate, function(err,result){
+  db_view.insert_view(userId, registerId, viewDate, registerTitle, function(err,result){
     if(err) console.log(err)
     else {
       var object = new Object()
@@ -41,6 +42,18 @@ router.post('/check', function(req, res, next){
         if(err) console.log(err)
         else res.send(result)
     })
+})
+
+router.post('/get', function(req, res, next){
+  var userId=req.body[0].user_id
+
+  db_view.get_view(userId, function(err, result) {
+    if(err) console.log(err)
+    else {
+      res.send(result)
+      console.log("뷰데이터 불러오기 완료")
+    }
+  })
 })
 
 module.exports = router;
